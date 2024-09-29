0757542b50c56f8a0499d6477c81de44ae59715f
muna <victorycollins31@gmail.com>
2024-09-29 19:31:36 +0100

    🔥 update: database user and root

--- resources/config.php
+++ resources/config.php
@@ -8,10 +8,24 @@ if (!defined("DS")) {
 
 // Check if the TEMPLATE_FRONT constant is not defined, and if so, define it to point to the "templates/back" directory.
 if (!defined("TEMPLATE_FRONT")) {
-    define("TEMPLATE_FRONT", __DIR__.DS. "templates/back");
+    define("TEMPLATE_FRONT", __DIR__.DS. "templates/front");
 }
 
+if (!defined("TEMPLATE_BACK")) {
+    define("TEMPLATE_BACK", __DIR__.DS. "templates/back");
+}
 
+if (!defined("DB_HOST")) {
+    define("DB_HOST",  "localhost");
+}
+
+if (!defined("DB_USER")) {
+    define("DB_USER",  "root");
+}
+
+if (!defined("DB_PASS")) {
+    define("DB_PASS",  "");
+}
 echo TEMPLATE_FRONT;
 
 
