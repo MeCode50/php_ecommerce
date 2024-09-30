7277633a12bcecd07e445ee3f95c78371e7c4569
muna <victorycollins31@gmail.com>
2024-09-30 12:32:49 +0100

    ✨ fetching category data from the database in side_nav

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,8 +1,25 @@
 <div class="col-md-3">
     <p class="lead">Shop Name</p>
     <div class="list-group">
-        <a href="category.html" class="list-group-item">Category 1</a>
-        <a href="#" class="list-group-item">Category 2</a>
-        <a href="#" class="list-group-item">Category 3</a>
+
+        <?php
+
+        $query =  "SELECT * FROM categories";
+        $send_query = mysqli_query($connection , $query);
+
+        while($row = mysqli_fetch_array($send_query)) {
+
+            echo "<a href='#' class='list-group-item'>{$row['cart_title']}</a>";
+
+        }
+
+
+
+        ?>
+
+
+
+
+       
     </div>
 </div>
